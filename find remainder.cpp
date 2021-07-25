6b1d80f095258be1943fdb1273cbdd1385b35c44
RITA-HITECH <ritabindal63@gmail.com>
2021-07-25 22:26:26 +0530

    The first line contains an integer T, the total number of test cases. Then T lines follow, each line contains two Integers A and B

---
+++ find remainder.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main()
+{
+    int T,A,B,C;
+    cin>>T;
+    for(int i=0;i<T;i++)
+    {
+        cin>>A;
+        cin>>B;
+    
+        C=A%B;
+    
+        
+    
+    cout<<C<<endl;
+    }
+   
+
+    return 0;
+}
